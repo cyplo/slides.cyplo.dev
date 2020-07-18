ac8cafdb2f980616d87404b928ea2434df189058
Cyryl Płotnicki <cyplo@cyplo.net>
2020-07-18 11:45:44 +0100

    add test talks

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Slide decks for talks by Cyryl Płotnicki" />
-    <h1>Slide decks for talks by Cyryl Płotnicki</h1>>
     <Link to="/hello/">Hello</Link> <br />
+    <Link to="/hello-copy/">Hello copy</Link> <br />
   </Layout>
 )
 
